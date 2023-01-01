ad98c843688cc3213b7634f87a90ce745c8b9c74
ihsaanul hoque <ihsaanulhoque@gmail.com>
2023-01-01 16:56:23 +0000

    minified code

---
+++ dist/index.js
@@ -0,0 +1,2 @@
+(()=>{var t=t||[];t.push(function(){let o=document.querySelector("div.tc_text-wrapper"),e=document.querySelector("input[type=submit]"),c=document.querySelector("span.watch"),n=new IntersectionObserver(r,{root:o,threshold:1});function r(s){s[0].intersectionRatio===1?e.disabled=!1:e.disabled=!0}n.observe(c)});})();
+//# sourceMappingURL=index.js.map
